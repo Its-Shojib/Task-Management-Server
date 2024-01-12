4ef5e190e27bd64c736c5a056232939da325c16b
Its_Shojib <mdshojib922@gmail.com>
2024-01-12 19:29:31 +0600

    Domain Changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://iridescent-florentine-b98e8c.netlify.app'
+        'https://task-master-web.netlify.app'
     ],
     credentials: true
 }));
